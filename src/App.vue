dff611b04eb89d4a499ce3699664f31d8d58866d
Gaétan <gmartin@weborama.com>
2021-02-19 16:39:05 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormEnt />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormEnt from "./components/FormEnt.vue"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    FormEnt
   }
 }
 </script>
---
+++ src/components/FormEnt.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+    .form
+        select.selct-type.formElement(v-model="form.TypeId")
+            option(:value="null") Please select a enterprise type
+            option(:value="t.ID" v-for="t in types") {{t.Name}}
+        input.input.formElement(v-model="form.Denomination" placeholder="Denomination") 
+        input.input.formElement(v-model="form.Siret" placeholder="Siret") 
+        input.input.long.formElement(v-model="form.Adress" placeholder="Adress") 
+        input.input.long.formElement(type="number" v-model.number="form.Revenue" placeholder="Revenue") 
+        button.button.formElement(@click="estimate") Estimate
+        span.result.formElement Tax estimated : {{tax}}
+</template>
+<script>
+export default {
+  name: "FormEnt",
+  data() {
+    return {
+      form: {
+        Denomination: "",
+        Siret: "",
+        Adress: "",
+        Revenue: 0,
+        TypeId: null
+      },
+      types: [],
+      tax: 0
+    }
+  },
+  async mounted() {
+    const res = await fetch("http://localhost:3010/types")
+    const data = await res.json()
+    this.types = data
+  },
+  methods: {
+    async estimate() {
+      const { form } = this
+      const res = await fetch("http://localhost:3010/estimate", {
+        method: "post",
+        body: JSON.stringify(form)
+      })
+      const data = await res.json()
+      this.tax = data
+    }
+  }
+}
+</script>
+<style lang="scss">
+.form {
+  width: 50%;
+  padding: 25%;
+  display: flex;
+  flex-direction: column;
+  .formElement {
+    margin-bottom: 10px;
+  }
+}
+</style>
