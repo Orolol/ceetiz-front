73c8fdaf55b4a0a040b4f4f60c862ea0f50f8d4d
Gaétan <gmartin@weborama.com>
2021-02-19 16:53:12 +0100

    add red border on mandatory

--- src/components/FormEnt.vue
+++ src/components/FormEnt.vue
@@ -3,9 +3,9 @@
         select.select-type.formElement(v-model="form.TypeId")
             option(:value="null") Please select a enterprise type
             option(:value="t.ID" v-for="t in types") {{t.Name}}
-        input.formElement(v-model="form.Denomination" placeholder="Denomination") 
-        input.formElement(v-model="form.Siret" placeholder="Siret") 
-        input.formElement(v-model="form.Adress" placeholder="Adress") 
+        input.formElement(:class="{'isError': errors.Denomination }" v-model="form.Denomination" placeholder="Denomination") 
+        input.formElement(:class="{'isError': errors.Siret }" v-model="form.Siret" placeholder="Siret") 
+        input.formElement(:class="{'isError': errors.Adress }" v-model="form.Adress" placeholder="Adress") 
         input.formElement(type="number" v-model.number="form.Revenue" placeholder="Revenue") 
         button.formElement(@click="estimate") Estimate
         span.formElement Tax estimated : {{tax}}
@@ -22,6 +22,7 @@ export default {
         Revenue: 0,
         TypeId: null
       },
+
       types: [],
       tax: 0
     }
@@ -30,6 +31,24 @@ export default {
     const res = await fetch("http://localhost:3010/types")
     const data = await res.json()
     this.types = data
+  },
+  computed: {
+    errors() {
+      const { types, form } = this
+      let errors = {
+        Denomination: false,
+        Siret: false,
+        Adress: false,
+        Revenue: false
+      }
+      if (types && form.TypeId) {
+        let type = types.find((t) => t.ID == form.TypeId)
+        for (let rule of type.Mandatory) {
+          if (!form[rule]) errors[rule] = true
+        }
+      }
+      return errors
+    }
   },
   methods: {
     async estimate() {
@@ -52,6 +71,9 @@ export default {
   flex-direction: column;
   .formElement {
     margin-bottom: 10px;
+  }
+  .isError {
+    border: 1px solid red;
   }
 }
 </style>
